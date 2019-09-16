a0ddc89e62271238297ea0b2b8b03e62956da0da
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-16 14:41:01 +0200

    Import devtools and add enhancement

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
 import reducers from '../reducers/index';
+import DevTools from '../DevTools';
+import { getCountries } from '../actions/actions-countries';
 
-const store = createStore(reducers);
+const store = createStore(reducers, DevTools.instrument());
+
+store.dispatch(getCountries());
 
 export default store;
