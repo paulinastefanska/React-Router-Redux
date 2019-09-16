fad497887d914fb84176240bb02e34c70fb91b61
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-16 14:46:20 +0200

    Add devtools to app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './store/index';
+import DevTools from './DevTools';
 
 render(
 	<Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
+	    <div>
+            <h1>Inicjalizacja projektu</h1>
+            <DevTools />
+        </div>
     </Provider>,
     document.getElementById('root')
 );
