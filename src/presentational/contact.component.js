3caa7f88f2ff8ca6f74c088ead623417554c610c
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-17 11:24:56 +0200

    Add contact component

---
+++ src/presentational/contact.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => (
+    <h1>Countries App Company</h1>
+    <adress>
+    	331 E Evelyn Ave<br>
+    	Mountain View, CA 94041<br>
+    	USA
+    </adress>
+
+);
+
+export default Contact;
