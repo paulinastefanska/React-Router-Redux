3e45fd18bf90a59e0e796d383547acab2b46da83
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-17 10:29:57 +0200

    Add react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import DevTools from './DevTools';
+import Navigation from './presentational/navigation.component';
 
 render(
 	<Provider store={store}>
 	    <div>
-            <h1>Inicjalizacja projektu</h1>
+	    	<Router history={hashHistory}>
+	            <Route path='/' component={Navigation}>
+
+	            </Route>
+        	</Router>
             <DevTools />
         </div>
     </Provider>,
