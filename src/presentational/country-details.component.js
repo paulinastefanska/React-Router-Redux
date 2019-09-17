d1dadbb00202155c0cf3aa0ad8aeb65205fbebbd
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-17 15:36:24 +0200

    Add styles

--- src/presentational/country-details.component.js
+++ src/presentational/country-details.component.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import styles from '../country.css';
 
 const CountryDetails = (props) => (
-    <div className="country-wrapper">
-        <header>
-            <img className="country-photo" src={props.country.imageUrl} alt="country pic" />
+    <div className="country-wrapper" style={styles}>
+        <header style={styles}>
+            <img className="country-photo" src={props.country.imageUrl} style={styles} alt="country pic" />
         </header>
-        <div className="country-info">
+        <div className="country-info" style={styles}>
             <h1>{props.country.name}</h1>
             <h2>Kontynent: {props.country.continent}</h2>
 
-            <div className="info">
+            <div className="info" style={styles}>
                 <div>
                     <span>{props.country.populace}</span>
                     <span>Ludność[mln]</span>
