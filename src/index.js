7e0e01ceaf160ad34071840827e255c8ab0e402c
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-17 10:37:53 +0200

    Import routes from separate file, remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/index';
+import routes from './routes';
 import DevTools from './DevTools';
-import Navigation from './presentational/navigation.component';
 
 render(
 	<Provider store={store}>
 	    <div>
-	    	<Router history={hashHistory}>
-	            <Route path='/' component={Navigation}>
-
-	            </Route>
-        	</Router>
+	    	<Router history={hashHistory} routes={routes} />
             <DevTools />
         </div>
     </Provider>,
